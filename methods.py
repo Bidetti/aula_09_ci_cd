5b851f8d95161815a758b2e40fee8c0a292dd828
Bidetti <rafael.bidetti@gmail.com>
2023-05-08 09:02:03 -0300

    Teste e funções da Calculadora
    Adicionado os testes e funções de: soma, subtração, divisão e multiplicação

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def soma(width, height):
+    soma = width + height
+    return soma
+
+def subtracao(width, height):
+    sub = width - height
+    return sub
+
+def multiplicacao(width, height):
+    mult = width * height
+    return mult
+
+def divisao(width, height):
+    div = width / height
+    return div
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_soma():
+    # given a width of 2 and a height of 5
+    width = 2
+    height = 5
+
+    # when we calculate the perimeter
+    output = methods.soma(width, height)
+    
+    # then the sum should be 7
+    assert output == 7
+
+def test_subtracao():
+    # given a width of 2 and a height of 5
+    width = 2
+    height = 5
+
+    # when we calculate the perimeter
+    output = methods.subtracao(width, height)
+    
+    # then the sub should be -3
+    assert output == -3
+
+def test_multiplicacao():
+    # given a width of 2 and a height of 5
+    width = 2
+    height = 5
+
+    # when we calculate the perimeter
+    output = methods.multiplicacao(width, height)
+    
+    # then the mult should be 10
+    assert output == 10
+
+def test_divisao():
+    # given a width of 2 and a height of 5
+    width = 2
+    height = 5
+
+    # when we calculate the perimeter
+    output = methods.divisao(width, height)
+    
+    # then the div should be 0.4
+    assert output == 0.4
